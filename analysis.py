2891ba3a23c627c1bd38f4619d48325b5e5f2808
RajMujmer <rajat.mujmer0206@gmail.com>
2025-05-20 14:17:58 +0530

    analysis.py

--- analysis.py
+++ analysis.py
@@ -7,7 +7,8 @@ from bs4 import BeautifulSoup
 import os
 import pickle  # Import pickle
 
-
+ working_dir = os.path.dirname(os.path.abspath(__file__))
+ file_path = working_dir/StopWords.txt
 def load_words(file_path: str, encoding: str = "utf-8") -> List[str]:
     """Loads words from a text file into a list. Handles encoding issues.
 
@@ -17,7 +18,7 @@ def load_words(file_path: str, encoding: str = "utf-8") -> List[str]:
     Returns:
         List[str]:  List of words, or an empty list on error.
     """
-    working_dir = os.path.dirname(os.path.abspath(__file__))
+   
 
     try:
         with open(file_path, "r", encoding=encoding) as f:
